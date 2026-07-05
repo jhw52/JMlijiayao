7b9d98851f3a949df049b88a926ed7f8447b43c3
jhw52 <W158760035942023@outlook.com>
2026-07-06 02:03:44 +0800

    Configure GitHub Pages base

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
   // If this is deployed to a root-domain repository such as username.github.io,
   // keep base as "/". If the repository is not a root-domain repository,
   // change it to "/仓库名/" before building and deploying.
-  base: "/",
+  base: "/JMlijiayao/",
 });
